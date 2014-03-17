c4d75d216a97702657de3f4923aa017d4c91e7c9
Said Maadan <me@saidmaadan.com>
2014-03-17 15:40:07 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Afriwood::Application.config.secret_key_base = 'f2e92ede228ed6072bbe4195876a7b175509007ab86bd41d8ed1f4f6940dd813721cb15b9d9df46c844c0817383addc31257a98b35174d197af8232cc62b7748'
